e052768cb6921dcfff1e5e9131847c680ce45c65
chhajed23 <aryan.chhajed23@gmail.com>
2020-07-29 16:02:02 +0530

    database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,52 @@
 var ball;
+var db,pos,loc;
 
 function setup(){
     createCanvas(500,500);
+    db=firebase.database();
+    
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    loc=db.ref('Ball/Position');
+    loc.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+    if(pos!==undefined){
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
+}
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    db.ref('Ball/Position').set({
+        x:pos.x+x,
+        y:pos.y+y
+    });
+
+   
+}
+
+function readPosition(data){
+    pos=data.val();
+    ball.x=pos.x;
+    ball.y=pos.y;
+}
+
+function showError(error){
+    console.log("error: "+ error);
 }
